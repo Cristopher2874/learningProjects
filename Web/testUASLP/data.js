e8917be53d8c8820dd142d3213a1e162fe4c09d4
Cristopher Hernández <73862045+Cristopher2874@users.noreply.github.com>
2024-10-29 07:47:50 -0600

    First React

---
+++ Web/testUASLP/data.js
@@ -0,0 +1,3 @@
+const member = ['user1','user2','user3','user4'];
+
+module.exports = member;
---
+++ Web/testUASLP/dataBase.js
@@ -0,0 +1,9 @@
+const members = ['user1','user2','user3','user4'];
+const registered = 4;
+const unregistered = 0;
+
+module.exports = { //nombre de propiedad + valor
+    members:members,
+    registered: registered,
+    notHere: unregistered
+};
